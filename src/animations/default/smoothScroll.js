74bcb2cc5357ec599a9d364b4028e4e7bdd9928a
us0610 <ujjwal158b@gmail.com>
2023-05-24 17:02:06 +0530

    made smooth touch 0

--- src/animations/default/smoothScroll.js
+++ src/animations/default/smoothScroll.js
@@ -8,7 +8,7 @@ export const smoothScroll = () => {
   const smoother = new ScrollSmoother.create({
     smooth: 1.3, // how long (in seconds) it takes to "catch up" to the native scroll position
     effects: true, // looks for data-speed and data-lag attributes on elements
-    smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    smoothTouch: 0, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
     normalizeScroll: true,
   });
 
